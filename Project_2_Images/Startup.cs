9a95bc885ea25521beb12523460a18db36de9364
Tyrell Herbst <tyrellh589@gmail.com>
2021-11-16 01:41:27 +0200

    Add project files.

--- Project_2_Images/Startup.cs
+++ Project_2_Images/Startup.cs
@@ -24,6 +24,7 @@ namespace Project_2_Images
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,7 @@ namespace Project_2_Images
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +53,7 @@ namespace Project_2_Images
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
