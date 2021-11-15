d27dd5fedf509872f4cede53b2d003e385b23dc1
Tyrell Herbst <tyrellh589@gmail.com>
2021-11-16 01:21:02 +0200

    Add project files.

---
+++ Project_2_Images/Areas/Identity/Data/Project_2_ImagesUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Project_2_Images.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the Project_2_ImagesUser class
+    public class Project_2_ImagesUser : IdentityUser
+    {
+    }
+}
---
+++ Project_2_Images/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project_2_Images.Areas.Identity.Data;
+using Project_2_Images.Data;
+
+[assembly: HostingStartup(typeof(Project_2_Images.Areas.Identity.IdentityHostingStartup))]
+namespace Project_2_Images.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AythDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AythDbContextConnection")));
+
+                services.AddDefaultIdentity<Project_2_ImagesUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AythDbContext>();
+            });
+        }
+    }
+}
